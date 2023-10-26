bb275498a579a45844fc3cb581f5e063d0e2bfcd
Adrian Saldias <adrsalzam1313@gmail.com>
2023-10-26 13:44:11 -0400

    Pruebas unitarias de Cesar

--- Cifrados_SeguridadTests/metodos_CesarTests.cs
+++ Cifrados_SeguridadTests/metodos_CesarTests.cs
@@ -15,14 +15,76 @@ namespace Cifrados_Seguridad.Tests
         [TestMethod()]
         public void cifrarTest()
         {
-            metodos_Cesar encriptacion = new metodos_Cesar("holamundo", 5);
+            metodos_Cesar encriptacion = new metodos_Cesar("hola mundo", 5);
 
             // Act (acción)
             string textoCifrado = encriptacion.cifrar();
 
             // Assert (verificación)
-            Assert.AreEqual("anotas el resultado que tendria que salir ", textoCifrado);
-            Assert.Fail();
+            Assert.AreEqual("MTQFRZSIT", textoCifrado);
+        }
+
+        [TestMethod()]
+        public void cifrarTestSinText()
+        {
+            metodos_Cesar encriptacion = new metodos_Cesar("", 5);
+
+            // Act (acción)
+            string textoCifrado = encriptacion.cifrar();
+
+            // Assert (verificación)
+            Assert.AreEqual("", textoCifrado);
+        }
+
+        [TestMethod()]
+        public void cifrarTextConCaracteresEspeciales()
+        {
+            metodos_Cesar encriptacion = new metodos_Cesar("H@llo", 5);
+
+            // Act (acción)
+            string textoCifrado = encriptacion.cifrar();
+
+            // Assert (verificación)
+            Assert.AreEqual("MQQT", textoCifrado);
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            // Arrange
+            metodos_Cesar desencriptacion = new metodos_Cesar("JXYFJXQFUWZJGFUFWFJQIJXHNKWFITIJHJXFW",5);
+
+            // Act
+            string textoDescifrado = desencriptacion.descifrar();
+
+            // Assert
+            Assert.AreEqual("ESTAESLAPRUEBAPARAELDESCIFRADODECESAR", textoDescifrado);
+        }
+
+        [TestMethod()]
+        public void DecryptTestSinTexto()
+        {
+            // Arrange
+            metodos_Cesar desencriptacion = new metodos_Cesar("", 5);
+
+            // Act
+            string textoDescifrado = desencriptacion.descifrar();
+
+            // Assert
+            Assert.AreEqual("", textoDescifrado);
+        }
+
+        [TestMethod()]
+        public void DescifrarConCaracteresEspeciales()
+        {
+            // Arrange
+            metodos_Cesar desencriptacion = new metodos_Cesar("O$LA@B", 5);
+
+            // Act
+            string textoDescifrado = desencriptacion.descifrar();
+
+            // Assert
+            Assert.AreEqual("JGVW", textoDescifrado);
         }
     }
 }
