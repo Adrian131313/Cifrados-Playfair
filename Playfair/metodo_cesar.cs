f9c7e906c559909cea4b529fee6cae0215855f0d
Michael Junior Camacho <michaeljunior2022@hotmail.com>
2023-10-25 23:02:24 -0400

    arreglos en el codigo cesar

--- Playfair/metodo_cesar.cs
+++ Playfair/metodo_cesar.cs
@@ -19,10 +19,39 @@ namespace Cifrados_Seguridad
             get { return textoCifrado; }
             set { textoCifrado = value; }
         }
+        private string RemoveAccentedCharacters(string text)
+        {
+            // Define los pares de reemplazo para letras acentuadas
+            var replacements = new Dictionary<char, char>
+    {
+        {'Á', 'A'},
+        {'Ñ', 'N'},
+        {'Í', 'I'},
+        {'Ó', 'O'},
+        {'Ú', 'U'},
+        {'Ü', 'U'},
+        {'ñ', 'n'},
+
+        // Agrega más pares según sea necesario
+    };
+
+            // Realiza el reemplazo de las letras acentuadas
+            foreach (var replacement in replacements)
+            {
+                text = text.Replace(replacement.Key, replacement.Value);
+            }
+
+            return text;
+        }
 
         public string cifrar()
         {
             textoCifrado = ""; // Inicializar la cadena de texto cifrado
+
+            // Normaliza el texto antes de cifrar
+            textoSinCifrar = RemoveAccentedCharacters(textoSinCifrar);
+            textoSinCifrar = textoSinCifrar.ToUpper(); // Convierte todo el texto a mayúsculas
+            textoSinCifrar = new string(textoSinCifrar.Where(char.IsLetter).ToArray()); // Elimina caracteres no alfabéticos
 
             foreach (char letra in textoSinCifrar)
             {
@@ -30,13 +59,9 @@ namespace Cifrados_Seguridad
                 {
                     char nuevaLetra = (char)(letra + clave);
 
-                    if (char.IsUpper(letra) && nuevaLetra > 'Z')
+                    if (nuevaLetra > 'Z')
                     {
                         nuevaLetra = (char)(nuevaLetra - 26); // Asegurar que permanezca en el rango de letras mayúsculas
-                    }
-                    else if (char.IsLower(letra) && nuevaLetra > 'z')
-                    {
-                        nuevaLetra = (char)(nuevaLetra - 26); // Asegurar que permanezca en el rango de letras minúsculas
                     }
 
                     textoCifrado += nuevaLetra;
@@ -54,19 +79,20 @@ namespace Cifrados_Seguridad
         {
             textoCifrado = ""; // Inicializar la cadena de texto descifrado
 
+            // Normaliza el texto antes de descifrar
+            textoSinCifrar = RemoveAccentedCharacters(textoSinCifrar);
+            textoSinCifrar = textoSinCifrar.ToUpper(); // Convierte todo el texto a mayúsculas
+            textoSinCifrar = new string(textoSinCifrar.Where(char.IsLetter).ToArray()); // Elimina caracteres no alfabéticos
+
             foreach (char letra in textoSinCifrar)
             {
                 if (char.IsLetter(letra)) // Solo descifrar letras
                 {
                     char nuevaLetra = (char)(letra - clave);
 
-                    if (char.IsUpper(letra) && nuevaLetra < 'A')
+                    if (nuevaLetra < 'A')
                     {
                         nuevaLetra = (char)(nuevaLetra + 26); // Asegurar que permanezca en el rango de letras mayúsculas
-                    }
-                    else if (char.IsLower(letra) && nuevaLetra < 'a')
-                    {
-                        nuevaLetra = (char)(nuevaLetra + 26); // Asegurar que permanezca en el rango de letras minúsculas
                     }
 
                     textoCifrado += nuevaLetra;
